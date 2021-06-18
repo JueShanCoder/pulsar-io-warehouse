d8d9db280d89ee0be5a4cb54acefaca4faf4e32f
JueShanCoder <lysjueshan@163.com>
2021-06-18 11:24:48 +0800

    添加pulsar-io-function

---
+++ pulsar-io-function/src/main/java/com/boxuegu/warehouse/functions/convert/ConvertConfig.java
@@ -0,0 +1,30 @@
+package com.boxuegu.warehouse.functions.convert;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.apache.pulsar.io.core.annotations.FieldDoc;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Data
+@Accessors(chain = true)
+public class ConvertConfig {
+
+    private static final long serialVersionUID = 1L;
+
+    @FieldDoc(
+            required = true,
+            defaultValue = "",
+            sensitive = true,
+            help = " snowflake clusterID "
+    )
+    private String convertTopicName;
+
+    public static ConvertConfig load(Map<String,Object> map) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(objectMapper.writeValueAsBytes(map), ConvertConfig.class);
+    }
+
+}
---
+++ pulsar-io-function/src/main/java/com/boxuegu/warehouse/functions/convert/ConvertFunction.java
@@ -0,0 +1,77 @@
+package com.boxuegu.warehouse.functions.convert;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.functions.api.Context;
+import org.apache.pulsar.functions.api.Function;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class ConvertFunction implements Function<byte[],byte[]> {
+
+    final ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    public byte[] process(byte[] bytes, Context context) {
+        try {
+            ConvertConfig convertConfig = ConvertConfig.load(context.getUserConfigMap());
+            if (convertConfig.getConvertTopicName() == null){
+                throw new IllegalArgumentException(" Required parameters are not set... Please check the startup script !!! ");
+            }
+            JsonNode jsonNode = convert2JsonNode(bytes);
+            // TODO will delete
+            log.info("====will delete, the jsonNode is {}",objectMapper.writeValueAsString(jsonNode));
+            Map<String, String> properties = new HashMap<>();
+            byte[] sinkBytes = convert2Byte(jsonNode,properties);
+            if (sinkBytes != null) {
+                log.info("send to convert topic, topic name is {}",convertConfig.getConvertTopicName());
+                context.newOutputMessage(convertConfig.getConvertTopicName(), Schema.BYTES).value(sinkBytes).properties(properties).send();
+            } else
+                log.info("[ConvertFunction sinkBytes is null, maybe 'op' is wrong ... ]");
+        } catch (Exception e){
+            log.error("[ConvertFunction got exception ..]",e);
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    private JsonNode convert2JsonNode(byte[] input) throws IOException {
+        return objectMapper.readTree(new String(input));
+    }
+
+    private byte[] convert2Byte(JsonNode jsonNode, Map<String, String> properties) throws JsonProcessingException {
+        String op = jsonNode.findValue("op").asText();
+        String databaseName = findDataBaseName(jsonNode);
+        String tableName = findTableName(jsonNode);
+        properties.put("TARGET",databaseName + "." + databaseName + "." + tableName);
+        properties.put("SQLMODE", "INSERT_IGNORE_INVALID");
+        if (op.equalsIgnoreCase("d")){
+            // delete operator
+            properties.put("ACTION","DELETE");
+            return objectMapper.writeValueAsString(jsonNode.get("before")).getBytes(StandardCharsets.UTF_8);
+        } else if (op.equalsIgnoreCase("u")){
+            // update operator
+            properties.put("ACTION","UPDATE");
+            return objectMapper.writeValueAsString(jsonNode.get("after")).getBytes(StandardCharsets.UTF_8);
+        } else if (op.equalsIgnoreCase("c")){
+            // insert operator
+            properties.put("ACTION","INSERT");
+            return objectMapper.writeValueAsString(jsonNode.get("after")).getBytes(StandardCharsets.UTF_8);
+        }
+        return null;
+    }
+
+    private String findDataBaseName(JsonNode jsonNode){
+        return jsonNode.get("source").findValue("db").asText();
+    }
+
+    private String findTableName(JsonNode jsonNode){
+        return jsonNode.get("source").findValue("table").asText();
+    }
+}
