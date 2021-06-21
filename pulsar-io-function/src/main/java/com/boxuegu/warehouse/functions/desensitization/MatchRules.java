a0e64d15421db53d6edfa509dcd29f0b069ab39d
JueShanCoder <lysjueshan@163.com>
2021-06-21 14:50:53 +0800

    修改无关引用

--- pulsar-io-function/src/main/java/com/boxuegu/warehouse/functions/desensitization/MatchRules.java
+++ pulsar-io-function/src/main/java/com/boxuegu/warehouse/functions/desensitization/MatchRules.java
@@ -1,7 +1,6 @@
 package com.boxuegu.warehouse.functions.desensitization;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import red.zyc.desensitization.Sensitive;
